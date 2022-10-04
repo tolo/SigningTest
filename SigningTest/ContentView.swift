fb998d9fefd81eb942159de9923861df71bdac67
Tobias Löfstrand <tobias@leafnode.se>
2022-10-04 15:02:57 +0200

    Initial Commit

---
+++ SigningTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SigningTest
+//
+//  Created by Tobias Löfstrand on 2022-10-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SigningTest/SigningTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SigningTestApp.swift
+//  SigningTest
+//
+//  Created by Tobias Löfstrand on 2022-10-04.
+//
+
+import SwiftUI
+
+@main
+struct SigningTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
